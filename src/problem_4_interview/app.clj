b5b3e3a27111514045b5c6ebfc7f64c3e8a8e94f
mik <mklimoff222@gmail.com>
2019-05-11 19:35:06 +0300

    init

---
+++ src/problem_4_interview/app.clj
@@ -0,0 +1,8 @@
+;; @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+;; @ Copyright (c) Michael Leahcim                                                      @
+;; @ You can find additional information regarding licensing of this work in LICENSE.md @
+;; @ You must not remove this notice, or any other, from this software.                 @
+;; @ All rights reserved.                                                               @
+;; @@@@@@ At 2019-05-11 19:32 <thereisnodotcollective@gmail.com> @@@@@@@@@@@@@@@@@@@@@@@@
+
+(ns problem_4_interview.app)
---
+++ src/problem_4_interview/handler.clj
@@ -0,0 +1,42 @@
+;; @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+;; @ Copyright (c) Michael Leahcim                                                      @
+;; @ You can find additional information regarding licensing of this work in LICENSE.md @
+;; @ You must not remove this notice, or any other, from this software.                 @
+;; @ All rights reserved.                                                               @
+;; @@@@@@ At 2019-05-11 19:32 <thereisnodotcollective@gmail.com> @@@@@@@@@@@@@@@@@@@@@@@@
+
+(ns problem_4_interview.handler
+  (:require [compojure.api.sweet :refer :all]
+            [ring.util.http-response :refer :all]
+            [schema.core :as s]))
+
+(s/defschema Pizza
+  {:name s/Str
+   (s/optional-key :description) s/Str
+   :size (s/enum :L :M :S)
+   :origin {:country (s/enum :FI :PO)
+            :city s/Str}})
+
+(def app
+  (api
+    {:swagger
+     {:ui "/"
+      :spec "/swagger.json"
+      :data {:info {:title "Problem_4_interview"
+                    :description "Compojure Api example"}
+             :tags [{:name "api", :description "some apis"}]}}}
+
+    (context "/api" []
+      :tags ["api"]
+
+      (GET "/plus" []
+        :return {:result Long}
+        :query-params [x :- Long, y :- Long]
+        :summary "adds two numbers together"
+        (ok {:result (+ x y)}))
+
+      (POST "/echo" []
+        :return Pizza
+        :body [pizza Pizza]
+        :summary "echoes a Pizza"
+        (ok pizza)))))
